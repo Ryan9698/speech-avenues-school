1691a417fbf102f2142fe41ec8b080be23028b9d
Ryan Caltabiano <rcaltabiano90@gmail.com>
2025-05-30 12:19:50 -0400

    Added a small change to the banner

--- satc-school/src/app/page.jsx
+++ satc-school/src/app/page.jsx
@@ -4,10 +4,21 @@ import { Mission } from '@/components';
 export default function Home() {
   return (
     <div className="space-y-6">
-      <div className="relative bg-sky-400 w-full h-96">
-        <p className="font-quicksand absolute top-3/4 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-3xl font-bold text-white font-geist">
-          Speech Avenues Therapy Co Communication Cultivation Academy
-        </p>
+      <div className="relative flex flex-col justify-center items-center md:flex-row bg-sky-100 w-full h-auto">
+        <div className="w-full md:w-1/2">
+          <p className="my-10 font-quicksand text-3xl font-bold text-gray-700 font-geist px-10 text-center">
+            Speech Avenues Therapy Co Communication Cultivation Academy
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/images/satcschool.webp"
+            alt="SATC Logo"
+            height={250}
+            width={500}
+            className="w-full h-full object-cover px-10"
+          />
+        </div>
       </div>
       <Mission />
     </div>
