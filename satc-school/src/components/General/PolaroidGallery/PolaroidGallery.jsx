dc9967f2bf9e4786d2d21bc7e5fe2340e3603b15
Ryan Caltabiano <rcaltabiano90@gmail.com>
2025-06-07 22:57:19 -0400

    Structural change to polaroid gallery and some styling corrections

--- satc-school/src/components/General/PolaroidGallery/PolaroidGallery.jsx
+++ satc-school/src/components/General/PolaroidGallery/PolaroidGallery.jsx
@@ -20,7 +20,7 @@ export default function PolaroidGallery({ photos = [] }) {
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-4 gap-y-8 mb-10">
         {photos.map((photo, index) => (
           <PolaroidCard
             key={photo.id || index}
